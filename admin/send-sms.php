ab79f8c0cf787d73e1cbac1a55bb19f64c08356c
Maithya <142247107+Maithy-a@users.noreply.github.com>
2024-12-13 03:07:43 +0300

    update 6 files

--- admin/send-sms.php
+++ admin/send-sms.php
@@ -267,22 +267,25 @@ if (isset($_POST['smsg'])) {
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php
+                                                                          $uname = $_SESSION['username'];
+                                                                          echo "<b><b>" . $uname . "</b></b>";
 
-                include "../conn.php";
-                $uname = $_SESSION['username'];
-                echo "<b><b>" . $uname . "</b></b>";
-
-                ?></span>
+                                                                          ?></span>
                 <img class="img-profile rounded-circle" src="../res/img/user.png">
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
 
+                <a class="dropdown-item" href="#">
+                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Settings
+                </a>
+                <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Logout
                 </a>
-
+              </div>
             </li>
 
           </ul>
